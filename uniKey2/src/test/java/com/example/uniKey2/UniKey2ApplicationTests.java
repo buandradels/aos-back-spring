f93e959c692d2cd0c6c847115996b3c465ca9e60
Bruna Cristina Andrade <38590890+buandradels@users.noreply.github.com>
2023-06-13 00:06:19 -0300

    Add files via upload

---
+++ uniKey2/src/test/java/com/example/uniKey2/UniKey2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.uniKey2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniKey2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
